0730aee25ee97e07714b5137ecad3d048ef6a06e
toon.beerten <toon.beerten@gmail.com>
2020-02-13 21:01:02 +0100

    please pull

--- VeryBadVirus.vb
+++ VeryBadVirus.vb
@@ -1,2 +1,18 @@
 dim virus
+
+msgbox virus
+haha virus
+msgbox virus
+msgbox virusmsgbox virusmsgboxmsgbox virus
+msgbox virus
+msgbox virusmsgbox
+msgbox virus
+msgbox virusmsgbox virusmsgbox
+msgbox virus
+msgbox virusmsgbox
+
+msgbox virus
+
+msgbox virusmsgbox
 'msgbox virus
+antivirus
