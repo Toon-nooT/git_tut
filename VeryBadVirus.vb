b2255471b893b45601eb753f423f15cc0b64c2e5
toon.beerten <toon.beerten@gmail.com>
2020-02-13 20:44:07 +0100

    extra virus

--- VeryBadVirus.vb
+++ VeryBadVirus.vb
@@ -1,2 +1,15 @@
 dim virus
 msgbox virus
+msgbox virus
+msgbox virus
+msgbox virusmsgbox virusmsgboxmsgbox virus
+msgbox virus
+msgbox virusmsgbox
+msgbox virus
+msgbox virusmsgbox virusmsgbox
+msgbox virus
+msgbox virusmsgbox
+
+msgbox virus
+
+msgbox virusmsgbox
